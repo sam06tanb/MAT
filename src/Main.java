eaa47547e059ea5e63b77888f91a5bfc96017323
sam06tanb <samirtajra53@gmail.com>
2023-11-08 16:21:23 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal: ");
+
+        for (int i=0; i<n; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Negative numbers: " + count);
+
+        sc.close();
+
+        }
+    }
